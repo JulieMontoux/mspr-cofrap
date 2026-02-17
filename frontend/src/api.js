d48479b13d74d504100ad3fa1ce963994e0d1654
JulieMontoux <julie.montoux@epsi.fr>
2026-02-17 19:12:24 +0100

    fix: path api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+  baseURL: "/function",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
-
